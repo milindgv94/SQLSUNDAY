2c635850495e1d23c8dc73e0bfe22bec18eb9911
milindgv94 <49666497+milindgv94@users.noreply.github.com>
2020-07-08 00:06:45 +0530

    MASTER

--- SQLFunctions/sql_basics_1.sql
+++ SQLFunctions/sql_basics_1.sql
@@ -1,11 +1,14 @@
 -- SIMPLE SELECT ALL STATEMENT --
+
 SELECT * FROM SalesLT.SalesOrderDetail
 
 -- SELECT SPECIFIC COLUMNS --
+
 SELECT	SalesOrderID, SalesOrderDetailID, OrderQty, ProductID, UnitPrice, UnitPriceDiscount, LineTotal
 FROM	SalesLT.SalesOrderDetail
 
 -- ALIASING TABLES SELECTED --
+
 SELECT	t1.SalesOrderID, 
 		t1.SalesOrderDetailID, 
 		t1.OrderQty, 
@@ -16,6 +19,7 @@ SELECT	t1.SalesOrderID,
 FROM	SalesLT.SalesOrderDetail t1
 
 -- SORTING DATA --
+
 SELECT	t1.SalesOrderID, 
 		t1.SalesOrderDetailID, 
 		t1.OrderQty, 
@@ -28,6 +32,7 @@ ORDER BY SalesOrderID desc
 
 -- TABLE JOINS --
 -- INNER JOIN --
+
 SELECT	t1.SalesOrderID, 
 		t1.SalesOrderDetailID, 
 		t1.OrderQty, 
@@ -52,6 +57,7 @@ INNER JOIN SalesLT.SalesOrderHeader t2
 ON		t1.SalesOrderID = t2.SalesOrderID
 
 -- RENAMING OUTPUT COLUMN NAMES / COMMENTING --
+
 SELECT	--t1.SalesOrderID, 
 --		t1.SalesOrderDetailID, 
 		t2.SalesOrderNumber AS [SO NUMBER],
@@ -71,6 +77,34 @@ SELECT	--t1.SalesOrderID,
 		t2.TaxAmt AS [TAX AMOUNT],
 		t2.Freight AS [FREIGHT AMOUNT],
 		t2.TotalDue AS [TOTAL DUE]
+FROM	SalesLT.SalesOrderDetail t1
+INNER JOIN SalesLT.SalesOrderHeader t2
+ON		t1.SalesOrderID = t2.SalesOrderID
+
+-- ADDING CALCULATED COLUMNS --
+-- GETDATE() RETURNS THE CURRENT SYSTEM DATE 
+
+SELECT	--t1.SalesOrderID, 
+--		t1.SalesOrderDetailID, 
+		t2.SalesOrderNumber AS [SO NUMBER],
+		t2.PurchaseOrderNumber AS [PO NUMBER],
+		t2.CustomerID AS [CUSTOMER CODE],
+		t2.ShipToAddressID AS [SHIPPING ADDRESS],
+		t2.BillToAddressID AS [BILLING ADDRESS],
+		t2.OrderDate AS [ORDER DATE],
+		t2.DueDate AS [DUE DATE],
+		t2.ShipDate AS [SHIPPED DATE],
+		t1.OrderQty AS [QUANTITY], 
+		t1.ProductID AS [PRODUCT CODE], 
+		t1.UnitPrice AS [UNIT PRICE], 
+		t1.UnitPriceDiscount AS [UNIT DISCOUNT], 
+		t1.LineTotal AS [LINE TOTAL],
+		t2.SubTotal AS [SUBTOTAL],
+		t2.TaxAmt AS [TAX AMOUNT],
+		t2.Freight AS [FREIGHT AMOUNT],
+		t2.TotalDue AS [TOTAL DUE],
+		t2.TaxAmt / t2.SubTotal * 100 AS [TAX %], -- CALCULATE TAX PERCENTAGE USING (TAX AMOUNT / ORDER AMOUNT) * 100
+		CASE WHEN t2.DueDate < GETDATE() THEN 'DUE' ELSE 'NOT DUE' END AS [DUE/NOT DUE] -- APPLIES AN IF CONDITION ON DUE DATE TO CHECK IF THE ORDER IS DUE OR NOT DUE
 FROM	SalesLT.SalesOrderDetail t1
 INNER JOIN SalesLT.SalesOrderHeader t2
 ON		t1.SalesOrderID = t2.SalesOrderID
